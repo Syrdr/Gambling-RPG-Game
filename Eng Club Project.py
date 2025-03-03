aa4a6ed6246639aa2df095efe99a571d288e4482
Syrdr <149530314+Syrdr@users.noreply.github.com>
2025-03-03 15:26:17 -0500

    Added main game

---
+++ Eng Club Project.py
@@ -0,0 +1,34 @@
+class Player:
+	def __init__(self, name, health, damage):
+  	self.name = name
+    self.health = health
+    self.damage = damage
+    self.isattacking = false
+  def Attack(dmg, enemyname):
+  	enemyname.health -= dmg
+  	self.isattacking = false
+playername = Input("Your name: ")
+currentplayer = new Player(playername, 100, 10)
+
+enemieslist = ["Monster1", "Monster2", "Monster3"]
+monsters = []
+
+
+class Enemy:
+	def __init__(self, name, health, damage):
+  	self.name = name
+    self.health = health
+    self.damage = damage
+  def TakeDamage(dmg):
+  	self.damage -= dmg
+i = 0
+h = 100
+for i in enemieslist:
+  Monster1 = new Enemy(i, h, 10 + h * 0.1)
+  h + =10
+  monsters.append(Monster1)
+  
+stage = 0
+def runGame():
+  
+  stage +=1
